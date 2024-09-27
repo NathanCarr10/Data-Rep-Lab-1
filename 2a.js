9c867fa72f1a4ba990a0712911caf5e6dc0564c0
unknown <nathancarr18@gmail.com>
2024-09-27 13:29:33 +0000

    2a.js

---
+++ 2a.js
@@ -0,0 +1,15 @@
+let a = function Hello(){
+
+    console.log("Data Representation & Querying");
+}
+
+a();
+
+let b = (value)=>{
+
+    console.log("Bye");
+    console.log(value);
+
+}
+
+b("Something");
