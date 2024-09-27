5893ce3024868b3023b99c15c944f28d3d6aa126
unknown <nathancarr18@gmail.com>
2024-09-27 13:50:14 +0000

    created function to add the sum of two numbers

---
+++ 2c.js
@@ -0,0 +1,12 @@
+
+//creating sum function to add two variables
+let sum = (a, b)=> {
+
+return(a+b)
+
+}
+
+//letting x return a value of numbers a + b
+let x=sum(25,50)
+//displaying answer on console
+console.log(x)
